7840d09d88b7237d07d4f1c61e9c59976dfe70ac
tonytran97 <tonytran_97@live.com>
2022-07-22 22:11:53 -0700

    prompt alerts for character length and special character request

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  let length = window.prompt('Please decide on the length of your password, it must be at least 8 characters long and no more than 128 characters.');
+  let special_characters = prompt('Would you like special characters to be included in your password?');
+  // include if function to detect if the client would like special characters or not
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+  // passwordText.value = password;
 
 }
 
