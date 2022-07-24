8080196bc3cd8b9979befe70bda3e9f8cc4b1322
tonytran97 <tonytran_97@live.com>
2022-07-23 18:27:34 -0700

    prompt selection criteria algorithm process

--- Develop/script.js
+++ Develop/script.js
@@ -14,10 +14,10 @@ function userprompts() {
     alert("Selection of length must be between 8 and 128 characters");
     return;
   }
-  lowercase = prompt('Would you like to include lowercase letters? (Y/N)');
-  uppercase = prompt('Uppercase? (Y/N)');
-  numbers = prompt('Numbers? (Y/N)');
-  special_characters = prompt('Special Characters (such as !@#$%^&*)? (Y/N)');
+  lowercase = confirm('Would you like to include lowercase letters? (Ok = Yes, Cancel = No');
+  uppercase = confirm('Uppercase? (Ok = Yes, Cancel = No)');
+  numbers = confirm('Numbers? (Ok = Yes, Cancel = No)');
+  special_characters = confirm('Special Characters (such as !@#$%^&*)? (Ok = Yes, Cancel = No)');
   // ensures that the user selects at least one of the criteria or else generation of password is terminated.
   if (!lowercase && !uppercase && !numbers && !special_characters) {
     alert("You must select at least one of the character types to be included in the password");
@@ -35,7 +35,39 @@ var charSetSelection = {
 // Write password to the #password input
 function writePassword() {
   userprompts();
-  // include if function to detect if the client would like special characters or not
+  // one criteria selection
+  if (lowercase) {
+    selection = lowerCharset;
+  } else if (uppercase) {
+    selection = upperCharset;
+  } else if (numbers) {
+    selection = numbersCharset;
+  } else if (special_characters) {
+    selection = specialCharset;
+    // two criteria selection 
+  } else if (lowercase && uppercase) {
+    selection = lowerCharset.concat(upperCharset);
+  } else if (lowercase && numbers) {
+    selection = lowerCharset.concat(numbersCharset);
+  } else if (lowercase && special_characters) {
+    selection = lowerCharset.concat(specialCharset);
+  } else if (uppercase && numbers) {
+    selection = upperCharset.concat(numbersCharset);
+  } else if (uppercase && special_characters) {
+    selection = upperCharset.concat(specialCharset);
+  } else if (numbers && special_characters) {
+    selection = numbersCharset.concat(specialCharset);
+    // three criteria selection
+  } else if (lowercase && uppercase && numbers) {
+    selection = lowerCharset.concat(upperCharset, numbersCharset);
+  } else if (lowercase && uppercase && special_characters) {
+    selection = lowerCharset.concat(upperCharset, specialCharset);
+  } else if (uppercase && numbers && special_characters) {
+    selection = upperCharset.concat(numbersCharset, specialCharset);
+    // all four criteria selection
+  } else if (lowercase && uppercase && numbers && special_characters) {
+    selection = lowerCharset.concat(upperCharset, numbersCharset, specialCharset);
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
