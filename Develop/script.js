771a42cc9e0eaa4b669e13f38c40f57cae4b1f72
tonytran97 <tonytran_97@live.com>
2022-07-25 11:30:53 -0700

    code clean up, comments to understand functions of code

--- Develop/script.js
+++ Develop/script.js
@@ -6,12 +6,12 @@ var generateBtn = document.querySelector("#generate");
 // this lists out all the prompts for the user to answer.
 function userprompts() {
   length = prompt('Please decide on the length of your password, it must be at least 8 characters long and no more than 128 characters.');
-  //  ensures that the user selects on a password length within parameters or else generation of password is terminated.
+  //  ensures that the user selects something and that the length is within parameters or else generation of password is terminated.
   if (!length) {
-    alert("Selection of length is required in order to proceed");
+    alert("Selection of length is required in order to proceed.");
     return;
   } else if (length < 8 || length > 128) {
-    alert("Selection of length must be between 8 and 128 characters");
+    alert("Selection of length must be between 8 and 128 characters.");
     return;
   }
   lowercase = confirm('Would you like to include lowercase letters? (Ok = Yes, Cancel = No');
@@ -20,24 +20,22 @@ function userprompts() {
   special_characters = confirm('Special Characters (such as !@#$%^&*)? (Ok = Yes, Cancel = No)');
   // ensures that the user selects at least one of the criteria or else generation of password is terminated.
   if (!lowercase && !uppercase && !numbers && !special_characters) {
-    alert("You must select at least one of the character types to be included in the password");
+    alert("You must select at least one of the character types to be included in the password.");
     return;
   }
 }
 
-// list of characters for selection 
-
+// arrays for character selection criteria 
 var charSetSelection = {
   lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
   numbersSelect: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  // !!difficulty inserting some symbols!!
   special: ["!", "#", "$", "%", "&", "*", "+", ",", "-", "<", ">", "?"],
 };
 
 function generatePassword() {
   userprompts();
-  // all four criteria selection
+  // all four criteria selection, must start from the broadest and narrow down to the single criteria for algorithm to work
   if (lowercase && uppercase && numbers && special_characters) {
     selection = charSetSelection.lower.concat(charSetSelection.upper, charSetSelection.numbersSelect, charSetSelection.special);
     // three criteria selection
@@ -68,16 +66,17 @@ function generatePassword() {
   } else if (special_characters) {
     selection = charSetSelection.special;
   };
-  // console.log(selection);
+
   ranPassGen = [];
+  // for loop will continually proceed to match the length provided by the user and will then return that generated password to ranPassGen
   for (var i = 0; i < length; i++) {
     choiceSelections = selection[Math.floor(Math.random() * selection.length)];
-    // console.log(choiceSelections);
+    // the following loops will be pushed towards the end of the array
     ranPassGen.push(choiceSelections);
-    // console.log(ranPassGen.join(''));
+    // the join method allows for the individual array elements to be combined into a single string
     newGeneratedPassword = ranPassGen.join('');
   };
-  return ranPassGen;
+  return;
 }
 
 // Write password to the #password input
